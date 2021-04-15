aad1b324e79868efc49b5ddbb4a3d82b7ad6f0a2
Thomas Pedersen <thomas@jekkit.no>
2021-04-15 09:20:11 +0200

    changed port nr again

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use("/campgrounds/:id/comments",commentRoutes);
 
 
 
-app.listen("3000", process.env.IP, function() {
+app.listen(process.env.PORT, process.env.IP, function() {
     console.log(process.env.PORT)
 });
 
