6e57f398805514a31e516abfcac0061a71fbc014
Thomas <t-pedersen@live.no>
2023-07-14 09:47:47 +0200

    Inital commit 2023

--- secrets.js
+++ secrets.js
@@ -1,7 +1,9 @@
 const   mongoose = require ("mongoose");
 
 function secrets(){
-var mongooseURI = mongoose.connect("mongodb+srv://dbJekkit:Jekkit4321@jekkitclutser.adply.mongodb.net/test?authSource=admin&replicaSet=atlas-abqo76-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", {useNewUrlParser: true,  useUnifiedTopology: true, useFindAndModify: false }
+    var mongooseURI = mongoose.connect("mongodb+srv://dbJekkit:Jekkit4321@jekkitcluster.fjac8eo.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true,  useUnifiedTopology: true, useFindAndModify: false }
 )};
 
 module.exports = secrets;
+
+
