d8ee2176c58d1e8526a15d1880b7a31036c5bb12
Thomas <t-pedersen@live.no>
2023-07-14 11:52:42 +0200

    test Azure key Vault

--- secrets.js
+++ secrets.js
@@ -1,8 +1,40 @@
 const   mongoose = require ("mongoose");
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
 
-function secrets(){
-    var mongooseURI = mongoose.connect("mongodb+srv://dbJekkit:Jekkit4321@jekkitcluster.fjac8eo.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true,  useUnifiedTopology: true, useFindAndModify: false }
-)};
+const vaultName = "jekkit-keyvault";
+const secretName = "mongooseURI";
+
+getUriFromKeyVault(vaultName, secretName)
+  .then((uri) => {
+    console.log("URI retrieved from Azure Key Vault:", uri);
+  })
+  .catch((error) => {
+    console.error("Error retrieving URI from Azure Key Vault:", error);
+  });
+
+
+
+async function secrets(vaultName, secretName) {
+  // Create a credential object using DefaultAzureCredential
+  const credential = new DefaultAzureCredential();
+
+  // Create a SecretClient to interact with Azure Key Vault
+  const vaultUrl = `https://${vaultName}.vault.azure.net/`;
+  const client = new SecretClient(vaultUrl, credential);
+
+  try {
+    // Get the secret value from Azure Key Vault
+    const secret = await client.getSecret(secretName);
+
+    // Return the URI
+    return secret.value;
+  } catch (error) {
+    console.error("Error retrieving secret from Azure Key Vault:", error);
+    throw error;
+  }
+}
+
 
 module.exports = secrets;
 
